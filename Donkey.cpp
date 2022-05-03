a9e35a92705749f89117eaaff829fc0e5aa28362
cplefka <cplefka@uvm.edu>
2022-05-03 13:50:16 -0400

    editing tutorial screen

--- Donkey.cpp
+++ Donkey.cpp
@@ -76,3 +76,43 @@ void Donkey::draw() const {
     }
     inFile.close();
 }
+
+void Donkey::drawFullDonkey() const {
+    // Don't forget to set the color to the fill field
+    int SIDE_LENGTH = sideLength;
+    ifstream inFile("../fullDonkey.txt");
+    inFile >> noskipws;
+    int xCoord = x, yCoord = y;
+    char letter;
+    bool draw;
+    while (inFile >> letter) {
+        draw = true;
+        switch(letter) {
+            case 'r': glColor3f(1, 0, 0); break;//red
+            case 'n' :glColor3f(.43, 1/2, 1/2); break;//brown
+            case 'g': glColor3f(0, 1, 0); break;//green
+            case 'b': glColor3f(0, 0, 0); break;//black
+            case 'y': glColor3f(1, 1, 0); break;//yellow
+            case 'm': glColor3f(1, 0, 1); break;//magenta
+            case 'c': glColor3f(0, 1, 1); break;//cyan
+            case 't': glColor3f(.5, .5, .5); break;//grey
+            case 'l': glColor3f(.3, .3, .3); break;//darker gray
+            case ' ': glColor4f(0.43, 0.32, 0.19,0); break;//dark brown background color
+
+            default: // newline
+                draw = false;
+                xCoord = x;
+                yCoord += SIDE_LENGTH;
+        }
+        if (draw) {
+            glBegin(GL_QUADS);
+            glVertex2i(xCoord, yCoord);
+            glVertex2i(xCoord+SIDE_LENGTH, yCoord);
+            glVertex2i(xCoord+SIDE_LENGTH, yCoord+SIDE_LENGTH);
+            glVertex2i(xCoord, yCoord+SIDE_LENGTH);
+            glEnd();
+            xCoord += SIDE_LENGTH;
+        }
+    }
+    inFile.close();
+}
